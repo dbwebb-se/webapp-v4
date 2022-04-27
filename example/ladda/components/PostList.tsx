9ec2130f5b27ba6fe3f4689f414089fababb3f51
Emil Folino <emilfolino@gmail.com>
2022-04-27 09:34:03 +0200

    removed imports

--- example/ladda/components/PostList.tsx
+++ example/ladda/components/PostList.tsx
@@ -1,14 +1,8 @@
 import { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 import { getPosts } from '../models/posts';
 import Post from '../interfaces/post';
-
-import MapView from 'react-native-maps';
-import { Marker } from "react-native-maps";
-
-import * as Location from 'expo-location';
-
 
 function distanceBetweenCoordinates(post, position) {
   const R = 6371e3; // metres
@@ -29,82 +23,18 @@ function distanceBetweenCoordinates(post, position) {
 
 export default function PostList() {
   const [posts, setPosts] = useState<Post[]>([]);
-  const [locationMarker, setLocationMarker] = useState(null);
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [currentGPSLocation, setCurrentGPSLocation] = useState(null);
 
   useEffect(async () => {
     setPosts(await getPosts());
-  }, []);
-
-  useEffect(() => {
-    (async () => {
-        const { status } = await Location.requestForegroundPermissionsAsync();
-
-        if (status !== 'granted') {
-            setErrorMessage('Permission to access location was denied');
-            return;
-        }
-
-        const currentLocation = await Location.getCurrentPositionAsync({});
-
-        setCurrentGPSLocation(currentLocation.coords);
-
-        setLocationMarker(<Marker
-            coordinate={{
-                latitude: currentLocation.coords.latitude,
-                longitude: currentLocation.coords.longitude
-            }}
-            title="Min plats"
-            pinColor="blue"
-        />);
-    })();
   }, []);
 
   const listOfPosts = posts.map((post, index) => {
     return <Text key={index} style={{fontSize: 40, marginBottom: 32}}>{post.identifier}: {post.name}</Text>
   });
 
-  const postMarkers = posts.map((post, index) => {
-    return <Marker
-                key={index}
-                coordinate={{ latitude: post.latitude, longitude: post.longitude }}
-                title={post.name}
-                onPress={() => console.log('pressed')}
-            />
-  });
-
-  if (posts[0] && currentGPSLocation) {
-    console.log(distanceBetweenCoordinates(
-      posts[0],
-      currentGPSLocation,
-    ));
-  }
-
   return (
-    <View style={styles.container}>
-      <MapView
-          style={styles.map}
-          initialRegion={{
-              latitude: 56.1612,
-              longitude: 15.5869,
-              latitudeDelta: 0.1,
-              longitudeDelta: 0.1,
-          }}>
-          {postMarkers}
-          {locationMarker}
-      </MapView>
-    </View>
+    <ScrollView>
+      {listOfPosts}
+    </ScrollView>
   );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "flex-end",
-        alignItems: "center",
-    },
-    map: {
-        ...StyleSheet.absoluteFillObject,
-    },
-});
